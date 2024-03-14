b67a7fb30f76daff70bfa1a0c64f81b4fcfbaedb
Arun Sudhakaran <85574637+aruns2008@users.noreply.github.com>
2024-03-14 11:35:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function msg() {
+  document.getElementById("show").style.top = "0px";
+}
+function msg2() {
+  document.getElementById("show").style.top = "-610px";
+}
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
