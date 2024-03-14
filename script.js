5b13ca924824eccdc498b2dc8b8707030ad5cbd9
Arun Sudhakaran <85574637+aruns2008@users.noreply.github.com>
2024-03-14 12:40:30 +0530

    changes

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// function msg() {
-//   document.getElementById("show").style.top = "0px";
-// }
-// function msg2() {
-//   document.getElementById("show").style.top = "-610px";
-// }
+function msg() {
+  document.getElementById("show").style.top = "0px";
+}
+function msg2() {
+  document.getElementById("show").style.top = "-610px";
+}
  
-// const inputs = document.querySelectorAll(".input");
+const inputs = document.querySelectorAll(".input");
 
-// function focusFunc() {
-//   let parent = this.parentNode;
-//   parent.classList.add("focus");
-// }
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
 
-// function blurFunc() {
-//   let parent = this.parentNode;
-//   if (this.value == "") {
-//     parent.classList.remove("focus");
-//   }
-// }
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
 
-// inputs.forEach((input) => {
-//   input.addEventListener("focus", focusFunc);
-//   input.addEventListener("blur", blurFunc);
-// });
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
